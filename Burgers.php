555f4b54cb5efdb20aa3df37e33ca39c73557715
josh671 <jmb671@hotmail.com>
2021-02-18 16:22:02 -0800

    fixed some css

--- Burgers.php
+++ Burgers.php
@@ -1,27 +1,81 @@
 <?php 
 
+// Burgers.php //
+
+/* EDIT PAGE TITLE HERE */
+
+$title="Bordallo's Burgers";
+
+/* end page title declaration */
+
+/* EDIT BURGER SELECTIONS BELOW */
+
+$burgers[]=new Burger(1, 'Steeler', "Delicious Burger made from the best Burger meats. - $5.75", 5.75, 'slider.jpg'); 
+$burgers[]=new Burger(2, 'Big Ben Burger', "Named after our QB and part owner. This burger has a lot of flavor. - $5.50", 5.50, 'bigBen.jpg'); 
+$burgers[]=new Burger(3, 'Beefy Beef Burger', "Great Deal for that amount of Beef - $9.50", 9.50, 'beefy.jpg'); 
+$burgers[]=new Burger(4,' Chicken Burger', "Fresh Frozen Chicken marked up 100% of its actual value. Yum! - $9.50", 9.50, 'chicken.jpg'); 
+$burgers[]=new Burger(5, 'Deal of the Day', "Get two of our delicious Burgers- $10.50", 10.50, 'double.jpg'); 
+$burgers[]=new Burger(6, 'Egg-Tacular', "Eggs and more Beef. Delicious. - $9.50", 9.50, 'egg.jpg'); 
+$burgers[]=new Burger(7, 'Yippy Skippy Burger', "Eggs and more Beef. Delicious. - $11.50", 11.50, 'egg.jpg'); 
+$burgers[]=new Burger(8, 'All Eggs and Bacon ', "Eggs and Bacon. What could Go wrong. - $11.50", 11.50, 'egg.jpg'); 
+$burgers[]=new Burger(9, 'Heart Attack Burger ', "Causes heartburn on first bite. - $3.50", 3.50, 'heart.jpg');
+/* end burger selections */
+
+/* ENTER ACTIVE TAX RATE(S) BELOW
+ENTER AS DECIMALS, EXAMPLE: 11.1 percent = 0.111 */
+
+$taxRate1 = 0.10;
+$totalTaxRate = $taxRate1;
+
+/* end active tax rate(s) */
+
+/* ADD ADDITIONAL TAX RATES IF NECESSARY
+EXAMPLE: 6.17 percent = 0.0617 */
+
+// $taxRate2 = 0.20;
+// $totalTaxRate = $totalTaxRate + $taxRate2;
+
+// $taxRate3 = 0.15;
+// $totalTaxRate = $totalTaxRate + $taxRate3;
+
+// etc.
+/* end tax rate(s) */
+
 class Burger { 
+    public $ID=0;
     public $name="";
     public $description="";
     public $imageName="";
     public $price=0;
-    public $quantity=0; 
+    public $quantity=0;
+    // $extras array not functioning with $burgers declared as array //
+    // public $extras = array();
 
-    public function __construct($name, $description, $price, $imageName){ 
-        $this->name = $name; 
+    public function __construct($ID, $name, $description, $price, $imageName){ 
+        $this->ID = $ID;
+        $this->name = $name;
         $this->description = $description; 
         $this->price = $price;
         $this->quantity = 0; 
         $this->imageName = $imageName; 
     }
-}//END BURGER CLASS 
 
-$burgers[]=new Burger('Steeler', "Delcious Burger made from the best Burger meats. - $5.75", 5.75, 'slider.jpg'); 
-$burgers[]=new Burger('Big Ben Burger', "Named after our QB and part owner. This burger has a lot of flavor. - $5.50", 5.50, 'bigBen.jpg'); 
-$burgers[]=new Burger('Beefy Beef Burger', "Great Deal for that amount of Beef - $9.50", 9.50, 'beefy.jpg'); 
-$burgers[]=new Burger('Cheecky Chicken Burger', "Fresh Frozen Chicken marked up 100% of its actual value. Yum! - $9.50", 9.50, 'chicken.jpg'); 
-$burgers[]=new Burger('Deal of the Day', "Get two of our delicious Burgers- $10.50", 10.50, 'double.jpg'); 
-$burgers[]=new Burger('Egg-Tacular', "Eggs and more Beef. Delicious. - $9.50", 9.50, 'egg.jpg'); 
-$burgers[]=new Burger('Yippy Skippy Burger', "Eggs and more Beef. Delicious. - $11.50", 11.50, 'egg.jpg'); 
-$burgers[]=new Burger('All Eggs and Baccon ', "Eggs and Baccon. What could Go wrong. - $11.50", 11.50, 'egg.jpg'); 
-$burgers[]=new Burger('Heart Attack Burger ', "Causes heartburn on first bite. - $3.50", 3.50, 'heart.jpg'); 
+// addExtra function not working with $burgers[] array //
+
+//     public function addExtra($extras){
+//         $this->extras[] = $extras;
+//     }// end addExtra //
+}// end Burger class //
+ 
+// extras might not work with $burgers declared  as an array:
+
+// $burgers = new Burger('All Eggs and Bacon ', "Eggs and Bacon. What could Go wrong. - $11.50", 11.50, 'egg.jpg');
+// $burgers->addExtra("Cheese");
+// $burgers->addExtra("Lard");
+// $burgers->addExtra("Pork rinds");
+// $burger[] = $burgers;
+// array conflict //
+
+// 2/14: Testing, code review, added commented code for addExtra function, declared $title, comments, minor edits. Joon 1.5 hr.
+
+//
